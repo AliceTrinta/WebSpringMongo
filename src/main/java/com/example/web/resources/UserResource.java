72825f63efdb74f92fa1124c2029ffab6797b63f
AliceTrinta <atg200210@outlook.com>
2020-07-27 14:40:12 -0300

    User entity created and REST APP working

--- src/main/java/com/example/web/resources/UserResource.java
+++ src/main/java/com/example/web/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.web.resources;
 
 import com.example.web.domain.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,12 @@ import java.util.List;
 public class UserResource {
 
     @GetMapping
-    public List<User> findAll(){
+    public ResponseEntity<List<User>> findAll(){
         User maria = new User("1", "Maria Brown", "maria@gmail.com");
         User alex = new User("2", "Alex Green", "alex@gmail.com");
         List<User> list = new ArrayList<>();
         list.addAll(Arrays.asList(maria, alex));
-        return list;
+        return ResponseEntity.ok().body(list);
     }
 
 }
