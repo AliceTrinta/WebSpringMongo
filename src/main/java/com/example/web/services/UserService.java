b0c32b88436cf195bc05d06c8e64d281db42c56d
AliceTrinta <atg200210@outlook.com>
2020-07-29 10:42:14 -0300

    User insert on database working

--- src/main/java/com/example/web/services/UserService.java
+++ src/main/java/com/example/web/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.web.services;
 
 import com.example.web.domain.User;
+import com.example.web.dto.UserDTO;
 import com.example.web.repositories.UserRepository;
 import com.example.web.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
