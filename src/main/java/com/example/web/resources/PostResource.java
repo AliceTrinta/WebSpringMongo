59b252dbaf9315f48b4507785302af8d98f86040
AliceTrinta <atg200210@outlook.com>
2020-07-29 14:48:51 -0300

    Finding posts by id

---
+++ src/main/java/com/example/web/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.example.web.resources;
+
+import com.example.web.domain.Post;
+import com.example.web.domain.User;
+import com.example.web.dto.UserDTO;
+import com.example.web.services.PostService;
+import com.example.web.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    PostService postService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = postService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/example/web/services/PostService.java
@@ -0,0 +1,21 @@
+package com.example.web.services;
+
+import com.example.web.domain.Post;
+import com.example.web.repositories.PostRepository;
+import com.example.web.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
