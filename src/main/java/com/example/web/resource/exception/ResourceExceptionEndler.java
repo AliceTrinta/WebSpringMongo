4f180e64236b546b6c15edbd3c76d09b820fc96a
AliceTrinta <atg200210@outlook.com>
2020-07-29 10:27:48 -0300

    UserDTO entity created and Exceptions treated

---
+++ src/main/java/com/example/web/resource/exception/ResourceExceptionEndler.java
@@ -0,0 +1,20 @@
+package com.example.web.resource.exception;
+
+import com.example.web.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionEndler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError er = new StandardError(System.currentTimeMillis(), status.value(), "NÃO ENCONTRADO", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(er);
+    }
+}
