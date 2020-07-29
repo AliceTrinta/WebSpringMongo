b8fcb9c3cf3b42894d817236359b30c03c7e2749
AliceTrinta <atg200210@outlook.com>
2020-07-29 15:38:51 -0300

    Using @query to search posts by title contet

--- src/main/java/com/example/web/repositories/PostRepository.java
+++ src/main/java/com/example/web/repositories/PostRepository.java
@@ -2,9 +2,13 @@ package com.example.web.repositories;
 
 import com.example.web.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String title);
     List<Post> findByTitleContaining(String text);
 }
